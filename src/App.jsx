d94e5eac7d0f5e899164c04d583afb417b6dc40b
CastCantoria <castcantoria@gmail.com>
2025-10-20 12:54:11 +0300

    Fix App.jsx with correct component names

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Footer from "./components/layout/Footer"
 import Home from "./pages/public/Home"
 import About from "./pages/public/About"
 import Repertoire from "./pages/public/Repertoire"
-import Events from "./pages/public/Events"
-import Gallery from "./pages/public/Gallery"
+import Concerts from "./pages/public/Concerts"
+import Galerie from "./pages/public/Galerie"
 import Spiritualite from "./pages/public/Spiritualite"
 import Blog from "./pages/public/Blog"
 import Contact from "./pages/public/Contact"
@@ -24,13 +24,13 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/repertoire" element={<Repertoire />} />
-              <Route path="/events" element={<Events />} />
-              <Route path="/gallery" element={<Gallery />} />
+              <Route path="/events" element={<Concerts />} />
+              <Route path="/gallery" element={<Galerie />} />
               <Route path="/spirituality" element={<Spiritualite />} />
               <Route path="/blog" element={<Blog />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/login" element={<Login />} />
-              <Route path="/admin" element={<div style={{ padding: "80px 20px 20px 20px", textAlign: "center" }}>Admin - En construction</div>} />
+              <Route path="/admin" element={<div style={{ padding: "80px 20px 20px 20px", textAlign: "center" }}>Administration - En construction</div>} />
             </Routes>
           </main>
           <Footer />
