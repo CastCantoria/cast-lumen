cd3122a1c328084c961fa53871cc813726b71016
CastCantoria <castcantoria@gmail.com>
2025-10-22 13:25:20 +0300

    fix: correct package.json, vite config and valid vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -20,6 +19,5 @@ export default defineConfig({
     port: 5173,
     host: true
   },
-  // Important pour Vercel
   base: './'
 })
